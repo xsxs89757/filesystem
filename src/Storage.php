12062a6abb0150705398ee09e2e80cd8e2543075
wanglei <317448423@qq.com>
2023-02-21 17:53:50 +0800

    增加base64上传

--- src/Storage.php
+++ src/Storage.php
@@ -162,6 +162,110 @@ class Storage {
     }
 
     /**
+     * 原文件覆盖
+     * @param $file
+     * @param $storageKey
+     * @return mixed
+     * @throws \Exception
+     */
+    public function reUpload($file,$fileName,$ext = false)
+    {
+        if(!empty($this->extYes) && !in_array($file->getUploadMineType(),$this->extYes)) {
+            throw new \Exception('不允许上传文件类型'.$file->getUploadMineType());
+        }
+        if(!empty($this->extNo) &&in_array($file->getUploadMineType(),$this->extNo)) {
+            throw new \Exception('文件类型不被允许'.$file->getUploadMineType());
+        }
+        if($file->getSize() > $this->size){
+            throw new \Exception("上传文件过大（当前大小 {$file->getSize()}，需小于 {$this->size})");
+        }
+        $filesystem = FilesystemFactory::get($this->adapterType);
+        $first = strrpos($fileName,'/');
+        if($first === false){
+            $path = $this->path;
+            $keyAndExt = explode('.',substr($fileName,0,strlen($fileName)));
+        }else{
+            $path = substr($fileName,0,$first);
+            $keyAndExt = explode('.',substr($fileName,$first + 1,strlen($fileName)));
+        }
+        $storageKey = $keyAndExt[0] ?? \hash_file('md5', $file->getPathname());
+        $fileName = $path.'/'.$storageKey.'.'.($ext?$keyAndExt[1]:$file->getUploadExtension());
+        if($filesystem->fileExists(trim($fileName, '/'))){
+            $filesystem->delete($fileName);
+        }
+        $result = $this->putFileAs($this->path, $file, $storageKey.'.'.($ext?$keyAndExt[1]:$file->getUploadExtension()));
+        $info = [
+            'origin_name' => $file->getUploadName(),
+            'file_name' => $fileName,
+            'storage_key' => $storageKey,
+            'file_url' => $this->url($fileName),
+            'size' => $file->getSize(),
+            'mime_type' => $file->getUploadMineType(),
+            'extension' => $file->getUploadExtension(),
+        ];
+        if (\substr($file->getUploadMineType(), 0, 5) == 'image') {
+            $size = \getimagesize($file);
+            $info['file_height'] = $size[1];
+            $info['file_width'] = $size[0];
+        }
+        return \json_decode(\json_encode($info));
+    }
+
+    /**
+     *
+     * @param $file
+     */
+    public function base64Upload($baseImg)
+    {
+
+        preg_match('/^(data:\s*image\/(\w+);base64,)/',$baseImg,$res);
+        if(count($res) != 3){
+           throw new \Exception('格式错误');
+        }
+        $img = base64_decode(str_replace($res[1],'', $baseImg));
+        $size = getimagesizefromstring($img);
+        if(count($size) == 0){
+            throw new \Exception('图片格式不正确');
+        }
+        if(!empty($this->extYes) && !in_array($size['mime'],$this->extYes)) {
+            throw new \Exception('不允许上传文件类型'.$size['mime']);
+        }
+        if(!empty($this->extNo) &&in_array($size['mime'],$this->extNo)) {
+            throw new \Exception('文件类型不被允许'.$size['mime']);
+        }
+
+        $filesystem = FilesystemFactory::get($this->adapterType);
+        $storageKey = md5(uniqid());
+        $fileName = $this->path.'/'.$storageKey.'.'.$res[2];
+        $base_img = str_replace($res[1], '', $baseImg);
+        $base_img = str_replace('=','',$baseImg);
+        $img_len = strlen($base_img);
+        $file_size = intval($img_len - ($img_len/8)*2);
+
+        if($file_size > $this->size){
+            throw new \Exception("上传文件过大（当前大小 {$file_size}，需小于 {$this->size})");
+        }
+
+        $this->put(
+            $path = trim($fileName, '/'), $img
+        );
+
+        $info = [
+            'origin_name' => $fileName,
+            'file_name' => $fileName,
+            'storage_key' => $storageKey,
+            'file_url' => $this->url($fileName),
+            'size' => $file_size,
+            'mime_type' => $size['mime'],
+            'extension' => $res[2],
+            'file_height' => $size[1],
+            'file_width' => $size[0]
+        ];
+
+        return \json_decode(\json_encode($info));
+    }
+
+    /**
     * 批量上传文件
     * @param $files
     * @param int $num
